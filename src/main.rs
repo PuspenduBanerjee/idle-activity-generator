623fef313f58fd965161235d0982e88d2ffa165e
Puspendu Banerjee <Puspendu.Banerjee@gmail.com>
2020-06-28 12:38:42 -0500

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,27 @@
 #[cfg(windows)]
 extern crate winapi;
 // use std::ffi::CString;
-use std::io::Error;
+// use std::io::Error;
 use std::thread;
 use std::time::Duration;
 
 use enigo::*;
 
 
-#[cfg(windows)]
-fn print_message(msg: &str) -> Result<i32, Error> {
-    use std::ffi::OsStr;
-    use std::iter::once;
-    use std::os::windows::ffi::OsStrExt;
-    use std::ptr::null_mut;
-    use winapi::um::winuser::{MB_OK, MessageBoxW};
-    let wide: Vec<u16> = OsStr::new(msg).encode_wide().chain(once(0)).collect();
-    let ret = unsafe {
-        MessageBoxW(null_mut(), wide.as_ptr(), wide.as_ptr(), MB_OK)
-    };
-    if ret == 0 { Err(Error::last_os_error()) }
-    else { Ok(ret) }
-}
+// #[cfg(windows)]
+// fn print_message(msg: &str) -> Result<i32, Error> {
+//     use std::ffi::OsStr;
+//     use std::iter::once;
+//     use std::os::windows::ffi::OsStrExt;
+//     use std::ptr::null_mut;
+//     use winapi::um::winuser::{MB_OK, MessageBoxW};
+//     let wide: Vec<u16> = OsStr::new(msg).encode_wide().chain(once(0)).collect();
+//     let ret = unsafe {
+//         MessageBoxW(null_mut(), wide.as_ptr(), wide.as_ptr(), MB_OK)
+//     };
+//     if ret == 0 { Err(Error::last_os_error()) }
+//     else { Ok(ret) }
+// }
 
 
 fn last_activity_time() -> u32 {
